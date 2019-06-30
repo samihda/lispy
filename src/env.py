2be86a9f74dc3056932d3aef87829176ca9bc899
samihda <samihda@users.noreply.github.com>
2019-06-30 14:14:03 +0200

    Add comparison symbols

--- src/env.py
+++ src/env.py
@@ -6,4 +6,18 @@ def global_env():
         '-': op.sub,
         '*': op.mul,
         '/': op.truediv,
+
+        '>': op.gt,
+        '<': op.lt,
+        '>=': op.ge,
+        '<=': op.le,
+        '=': op.eq,
+
+        'eq?': op.is_,
+        'equal?': op.eq,
+        'list?': lambda x: isinstance(x, list),
+        'null?': lambda x: x == [],
+        'number?': lambda x: isinstance(x, (int, float)),
+        'procedure?': callable,
+        'symbol?': lambda x: isinstance(x, str),
     }
